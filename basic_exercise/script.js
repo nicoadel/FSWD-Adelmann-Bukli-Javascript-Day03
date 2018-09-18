7771a764f1933adb9103320c53f3a2fe51141819
nicoarl <nico.adelmann@hotmail.com>
2018-09-18 09:56:47 +0200

    other basic exerise

--- basic_exercise/script.js
+++ basic_exercise/script.js
@@ -1,13 +1,25 @@
 temperature();
 function temperature() {
-	var randomNumber = (Math.floor(Math.random() * 31) - 5);
-	if ((-5 <= randomNumber) &&(randomNumber <=10))
-	{
-		document.write("The weather is cold");	
+    var randomNumber = (Math.floor(Math.random() * 31) - 5);
+    if ((-5 <= randomNumber) && (randomNumber <= 10)) {
+        document.write("The weather is cold");
 
-	}
-	else
-	{
-		document.write("The weather is moderate");	
-	}
+    } else {
+        document.write("The weather is moderate");
+    }
 }
+
+function highestarray(array) {
+    var number = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > number) { number = array[i] }
+    }
+
+}
+
+function product(array) {
+    var number = 1;
+    for (var i = 0; i < array.length; i++) { number *= array[i] }
+
+}
+
