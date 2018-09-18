7f5880866715148dc4aae26edbb4eb33b5da45cc
nicoarl <nico.adelmann@hotmail.com>
2018-09-18 15:16:24 +0200

    finished exercise kinda

---
+++ challenge_1/scripts/script.js
@@ -0,0 +1,15 @@
+function showItem1() {
+    document.getElementById("1_shoe").classList.toggle("show");
+}
+function showItem2() {
+    document.getElementById("2_shoe").classList.toggle("show");
+}
+function showItem3() {
+    document.getElementById("3_shoe").classList.toggle("show");
+}
+function showItem4() {
+    document.getElementById("4_shoe").classList.toggle("show");
+}
+function showItem5() {
+    document.getElementById("5_shoe").classList.toggle("show");
+}
