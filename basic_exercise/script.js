b228f9d1f1d94ef42feccb5de0bc7fac73a08520
nicoarl <nico.adelmann@hotmail.com>
2018-09-18 10:50:21 +0200

    finihsed basic

--- basic_exercise/script.js
+++ basic_exercise/script.js
@@ -1,25 +1,33 @@
+var numbers = [1, 4, 5, 10, 20]
+document.write(product(numbers));
 temperature();
+
 function temperature() {
     var randomNumber = (Math.floor(Math.random() * 31) - 5);
     if ((-5 <= randomNumber) && (randomNumber <= 10)) {
-        document.write("The weather is cold");
+        document.getElementById("output").innerHTML = "the weather is cold";
+        document.getElementById("outputimg").src = "cold.jpg";
 
     } else {
-        document.write("The weather is moderate");
+        document.getElementById("output").innerHTML = "the weather is moderate";
+        document.getElementById("outputimg").src = "warm.jpg";
     }
 }
 
 function highestarray(array) {
     var number = 0;
     for (var i = 0; i < array.length; i++) {
-        if (array[i] > number) { number = array[i] }
+        if (array[i] > number) {
+            number = array[i]
+        }
     }
 
 }
 
 function product(array) {
     var number = 1;
-    for (var i = 0; i < array.length; i++) { number *= array[i] }
-
+    for (var i = 0; i < array.length; i++) {
+        number *= array[i]
+    }
+    return (number);
 }
-
